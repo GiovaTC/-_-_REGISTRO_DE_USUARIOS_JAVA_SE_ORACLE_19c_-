7a1b8d10f27f2700cb4362305151600220f3c624
gtapiero <giovanny.tapiero@pi.edu.co>
2026-02-19 18:04:50 -0500

    commit - UserForm / .

--- src/dao/UserDAO.java
+++ src/dao/UserDAO.java
@@ -48,4 +48,4 @@ public class UserDAO {
         con.close();
         return list;
     }
-}   
+}
--- src/view/UserForm.java
+++ src/view/UserForm.java
@@ -1,4 +1,70 @@
 package view;
 
-public class UserForm {
+import dao.UserDAO;
+import model.User;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.util.List;
+
+public class UserForm extends JFrame {
+
+    private JTable table;
+    private DefaultTableModel model;
+
+    private JTextField[] fields = new JTextField[9];
+
+    public UserForm() {
+
+        setTitle("User Registry - oracle 19c");
+        setSize(900, 500);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        setLayout(new BorderLayout());
+
+        JPanel formPanel = new JPanel(new GridLayout(3, 6));
+
+        String[] labels = {
+                "ID","First Name","Last Name","Email",
+                "Phone","Username","Password","Role","Status"
+        };
+
+        for (int i = 0; i < 9; i++) {
+            formPanel.add(new JLabel(labels[i]));
+            fields[i] = new JTextField();
+            formPanel.add(fields[i]);
+        }
+
+        add(formPanel, BorderLayout.NORTH);
+
+        model = new DefaultTableModel(labels, 7);
+        table = new JTable(model);
+        add(new JScrollPane(table), BorderLayout.CENTER);
+
+        JPanel buttonPanel = new JPanel();
+
+        JButton btnCreate = new JButton("CREATE");
+        JButton btnRead = new JButton("READ");
+        JButton btnUpdate = new JButton("UPDATE");
+        JButton btnDelete = new JButton("DELETE");
+
+        buttonPanel.add(btnCreate);
+        buttonPanel.add(btnRead);
+        buttonPanel.add(btnUpdate);
+        buttonPanel.add(btnDelete);
+
+        add(buttonPanel, BorderLayout.SOUTH);
+
+        UserDAO dao = new UserDAO();
+
+        btnCreate.addActionListener(e -> execute("INSERT", dao));
+        btnRead.addActionListener(e -> execute("SELECT", dao));
+        btnUpdate.addActionListener(e -> execute("UPDATE", dao));
+        btnDelete.addActionListener(e -> execute("DELETE", dao));
+    }   
+
+    private void execute(String insert, UserDAO dao) {
+    }
 }
