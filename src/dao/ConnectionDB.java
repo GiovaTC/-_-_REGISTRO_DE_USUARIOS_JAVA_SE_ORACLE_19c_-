b89377cc9e40a2716b1eae82b18682acdb483d83
gtapiero <giovanny.tapiero@pi.edu.co>
2026-02-19 17:42:07 -0500

    commit - ConnectionDB / .

--- src/dao/ConnectionDB.java
+++ src/dao/ConnectionDB.java
@@ -1,4 +1,15 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class ConnectionDB {
+
+    private static final String URL = "jdbc:oracle:thin:@//localhost:1521/orcl";
+    private static final String USER = "system";
+    private static final String PASS = "Tapiero123";
+
+    public static Connection getConnection() throws Exception {
+        return DriverManager.getConnection(URL, USER, PASS);
+    }
 }
