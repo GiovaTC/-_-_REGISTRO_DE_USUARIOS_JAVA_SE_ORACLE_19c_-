e8cac9eaed859c6b98a3716eb144d9b81569d8ac
gtapiero <giovanny.tapiero@pi.edu.co>
2026-02-19 18:08:19 -0500

    commit - UserForm .

--- src/view/UserForm.java
+++ src/view/UserForm.java
@@ -63,8 +63,46 @@ public class UserForm extends JFrame {
         btnRead.addActionListener(e -> execute("SELECT", dao));
         btnUpdate.addActionListener(e -> execute("UPDATE", dao));
         btnDelete.addActionListener(e -> execute("DELETE", dao));
-    }   
-
-    private void execute(String insert, UserDAO dao) {
     }
+
+    private void execute(String action, UserDAO dao) {
+        try {
+            User u = new User();
+            u.setId(parseInt(fields[0].getText()));
+            u.setFirstName(fields[1].getText());
+            u.setLastName(fields[2].getText());
+            u.setEmail(fields[3].getText());
+            u.setPhone(fields[4].getText());
+            u.setUsername(fields[5].getText());
+            u.setPassword(fields[6].getText());
+            u.setRole(fields[7].getText());
+            u.setStatus(fields[8].getText());
+
+            List<User> list = dao.executeSP(action, u);
+
+            model.setRowCount(0);
+
+            for (User user : list) {
+                model.addRow(new Object[]{
+                        user.getId(),
+                        user.getFirstName(),
+                        user.getLastName(),
+                        user.getEmail(),
+                        user.getPhone(),
+                        user.getUsername(),
+                        user.getPassword(),
+                        user.getRole(),
+                        user.getStatus()
+                });
+            }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private int parseInt(String value){
+        try { return Integer.parseInt(value); }
+        catch (Exception e) { return 0; }
+    }   
 }
